24d1407faae8aa0b651ea88b515c5d12c7e3356b
Christian Schilling <christian.schilling@rub.de>
2008-06-09 00:16:05 +0200

    postmerge fix

--- node.py
+++ node.py
@@ -3,7 +3,7 @@
 class MenuNode:
     def __init__(self,path='',name='',title='',
                  in_menu=True,in_contents=True,in_index=True,
-                 show_contents=False):
+                 show_contents=False,heading=False):
         self.pathlist = path.strip('/').split('/')
         self.name = name
         self.title = title
@@ -12,6 +12,7 @@ class MenuNode:
         self.in_index = in_index
         self.in_contents = in_contents
         self.show_contents = show_contents
+        self.heading = heading
     def __unicode__(self):
         return u'%s - %s' %(self.name,self.path)
     __str__ = __unicode__
