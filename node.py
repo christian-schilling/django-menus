96b6f4f2da885b809ccf2b9d05e84feb8de0c5d3
Christian Schilling <christian.schilling@rub.de>
2008-06-08 17:18:40 +0200

    node eigenschaften erweitert

--- node.py
+++ node.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 
 class MenuNode:
-    def __init__(self,path='',name='',title='',in_menu=True):
+    def __init__(self,path='',name='',title='',
+                 in_menu=True,in_contents=True,in_index=True,
+                 show_contents=False):
         self.pathlist = path.strip('/').split('/')
         self.name = name
         self.title = title
         self.active = self.open = False
         self.in_menu = in_menu
+        self.in_index = in_index
+        self.in_contents = in_contents
+        self.show_contents = show_contents
     def __unicode__(self):
         return u'%s - %s' %(self.name,self.path)
     __str__ = __unicode__
