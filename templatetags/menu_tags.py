1091b622d514ed609dad8799d0884d4a78f150fc
Christian Schilling <christian.schilling@rub.de>
2008-06-08 18:38:26 +0200

    menugen templates verschoben

---
+++ templatetags/menu_tags.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from django import template
+from util.menus import MenuNode,main_menu
+
+register = template.Library()
+
+@register.inclusion_tag('menus/menu.html',takes_context=True)
+def menu(context):
+    curpath = context['request'].path
+    pathstart = MenuNode(curpath).pathstart
+    context.update({'nodes':[x for x in main_menu.children(pathstart)
+                            if x.in_menu]})
+    return context
+
+@register.inclusion_tag('menus/node.html',takes_context=True)
+def node(context,n,depth=1):
+    curpath = context['request'].path
+    branch = main_menu.branch(curpath)
+    children = [x for x in main_menu.children(n.path) if x.in_menu]
+    n.open = n.active = n.path in branch
+    for c in children:
+        n.active &= (c.path not in branch)
+    if not n.open or depth >= main_menu.depth:
+        context.update({'children':[]})
+    else:
+        context.update({'children':children})
+    context.update({'node':n,'depth':depth+1})
+    return context
+
+@register.inclusion_tag('menus/breadcrumbs.html',takes_context=True)
+def breadcrumbs(context):
+    curpath = context['request'].path
+    branchnodes = (main_menu.node(x) or MenuNode(x)
+                   for x in main_menu.branch(curpath)[1:])
+    context.update({'branch':branchnodes,})
+    return context
+
+@register.inclusion_tag('menus/tabbar.html',takes_context=True)
+def tabbar(context):
+    curpath = context['request'].path
+    nodes = [x for x in main_menu.children('/') if x.in_menu]
+    pathstart = MenuNode(curpath).pathstart
+    for node in nodes:
+        node.active = node.path == pathstart
+    context.update({'nodes':nodes})
+    return context
+
+@register.inclusion_tag('menus/contents.html',takes_context=True)
+def contents(context):
+    curpath = context['request'].path
+    branch = main_menu.branch(curpath)
+    for x in reversed(branch):
+        node = main_menu.node(x)
+        if node and node.show_contents:
+            context.update({'rootnode':node,'nodes':main_menu.children(x)})
+            break
+    return context
+
