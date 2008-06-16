bfc95cbf119088c2f08da758db2ccb12b58baf3b
Christian Schilling <christian.schilling@rub.de>
2008-06-17 00:40:12 +0200

    docu

--- menu.py
+++ menu.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 
 def my_import(name):
+    """
+    This is taken directly from the Python documentation.
+    It is a thin wrapper around the buildin __import__
+    function that that returns "eggs" instead of "span" when
+    importing "spam.eggs"
+    """
     mod = __import__(name)
     components = name.split('.')
     for comp in components[1:]:
@@ -8,17 +14,41 @@ def my_import(name):
     return mod
 
 class Menu:
+    """
+    This class is the high-level interface of the menugen application.
+    Is is used in places where the menu should be rendered (by the templatetags).
+    It holds a list of references to MenuGenerator instances and its interface methods
+    just "merge" the results of the corresponding MenuGenerator method together.
+    """
     def __init__(self,depth=1):
+        """
+        Just initializes self.generators as an empty tuple.
+        May take "depth" with is the number of levels of menu hierarchy
+        that should be shown when the menu is rendered.
+        """
         self.generators = ()
         self.depth = depth
 
     def addgenerator(self,gen,offset=0):
+        """
+        Adds a MenuGenerator instance "gen" to the references list.
+        If "gen" is a already initialized instance of MenuGenerator, it is
+        is added directly.
+        If it is a string, the module specified in it is imported and the contained
+        instance is added. The module specified should contain such an instance
+        just named "generator"
+        "offset" is a value that is to be added to the position of every MenuNode
+        of the generator before sorting them.
+        """
         if type(gen) == str:
             gen = my_import(gen).generator
         gen.offset = offset
         self.generators += (gen,)
 
     def node(self,path):
+        """
+
+        """
         for gen in self.generators:
             n = gen.node(path)
             if n: return n
